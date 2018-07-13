3f5d90ff412c8c6a4bd9ad05d31357f17ba9b4b4
CHENG-YOU LU <A2694815@gmail.com>
2018-07-14 03:25:39 +0800

    Update load.py

--- Simple_does_it/Dataset/load.py
+++ Simple_does_it/Dataset/load.py
@@ -48,8 +48,6 @@ class Load:
                     label = Image.open(self.dataset_path + '/' + self.label_dir_name + '/' + img_name + '.png')
                     label = label.resize((self.width, self.height), PIL.Image.NEAREST)
                     label = np.array(label)
-                    label = np.where((label > 21), 21, label)
-                    label = np.where((label < 0), 0, label)
                     # [w, h] to [w, h, 1] 
                     label = np.expand_dims(label, axis = 2)
                     self.y.append(label)
